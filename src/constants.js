dac171b616c3dd33f4d7d8d8a8c6b5d815f171a6
GauthamKumarasamy <Gautham.kumarasamy@kanini.com>
2024-08-11 23:04:09 +0530

    changes in git ignore

---
+++ src/constants.js
@@ -0,0 +1,50 @@
+
+export const services = [
+    {
+        title: "Technology Enthusiast",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+    {
+        title: "Software Engineer",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+    {
+        title: "Photographer",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+];
+
+export const name = 'Gautham ';
+
+export const experiences = [
+    {
+        'company': 'KANINI',
+        'role': 'Product Developer',
+        'duration': 'Nov 2023 - Present',
+        'logo': 'https://kanini.com/wp-content/uploads/2022/06/Kanini-Logo.svg',
+        'points': [
+            'Involved in dynamic contributions to web development projects, actively influencing and improving project outcomes.',
+            'Developed multiple shared components to simplify tasks for fellow developers, boosting efficiency and teamwork.',
+            'Enhanced product performance through significant contributions to backend development, optimizing essential components for improved efficiency 📈.',
+            'Implemented data security for the company’s product to provide role-based access control ⚙️.'
+        ],
+        'url': 'https://inncircles.com/#home',
+    },
+    {
+        'company': 'KANINI',
+        'role': 'Traine Asociate',
+        'duration': 'Feb 2023 - Nov 2023',
+        'logo': 'https://kanini.com/wp-content/uploads/2022/06/Kanini-Logo.svg',
+        'points': [
+            'Gained experience with Git version control contributing to the development and maintenance of code repositories.',
+            'Worked on the application dashboard, making it more user-friendly and improving its overall functionality📱.'
+        ],
+        'url': 'https://www.att.com/',
+    },
+]
+
+
+
+export const EMAIL_JS_SERVICE_ID = process.env.REACT_APP_EMAIL_JS_SERVICE_ID;
+export const EMAIL_JS_TEMPLATE_ID = process.env.REACT_APP_EMAIL_JS_TEMPLATE_ID;
+export const EMAIL_JS_PUBLIC_KEY = process.env.REACT_APP_EMAIL_JS_PUBLIC_KEY;
